1fca32c9053d77824aad53dec746a62afe2d20ec
Ivan Bachyla <Ivan_Bachyla@epam.com>
2016-12-15 15:49:54 +0300

    Change methods visibility for org.jbehave.core.embedder.Embedder

--- jbehave-core/src/main/java/org/jbehave/core/embedder/Embedder.java
+++ jbehave-core/src/main/java/org/jbehave/core/embedder/Embedder.java
@@ -230,7 +230,7 @@ public class Embedder {
         }
     }
 
-    private void handleFailures(BatchFailures failures) {
+    protected void handleFailures(BatchFailures failures) {
         if (failures.size() > 0) {
             if (embedderControls().ignoreFailureInStories()) {
                 embedderMonitor.batchFailed(failures);
@@ -416,7 +416,7 @@ public class Embedder {
      * 
      * @return An ExecutorService
      */
-    private ExecutorService createExecutorService() {
+    protected ExecutorService createExecutorService() {
         return new FixedThreadExecutors().create(embedderControls());
     }
 
@@ -441,7 +441,7 @@ public class Embedder {
         return storyManager;
     }
 
-    private StoryManager createStoryManager() {
+    protected StoryManager createStoryManager() {
         return new StoryManager(configuration(), stepsFactory(), embedderControls(), embedderMonitor(),
                 executorService(), performableTree(), timeoutParsers());
     }
