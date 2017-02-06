afb27d6732c6fa1eb71724dd52518f056162455a
aliaksandrts <Aliaksandr_Tsymbal@epam.com>
2017-02-06 13:20:43 +0300

    Enable multithreaded execution for story with examples table

--- jbehave-core/src/main/java/org/jbehave/core/configuration/Configuration.java
+++ jbehave-core/src/main/java/org/jbehave/core/configuration/Configuration.java
@@ -184,6 +184,8 @@ public abstract class Configuration {
      */
     protected List<String> compositePaths;
 
+    private boolean parallelStoryExampleTableEnabled;
+
     public Configuration() {
     }
 
@@ -461,5 +463,13 @@ public abstract class Configuration {
     public Configuration useCompositePaths(List<String> compositePaths) {
         this.compositePaths = compositePaths;
         return this;
+    }
+
+    public boolean isParallelStoryExampleTableEnabled() {
+        return parallelStoryExampleTableEnabled;
+    }
+
+    public void setParallelStoryExampleTableEnabled(boolean parallelStoryExampleTableEnabled) {
+        this.parallelStoryExampleTableEnabled = parallelStoryExampleTableEnabled;
     }
 }
