e9efc8583a03594ed371eee40454eec7690dad8f
Valery Yatsynovich <valfirst@yandex.ru>
2018-06-04 17:01:13 +0300

    Fix reporting of Lifecycle Before/After steps

--- jbehave-core/src/main/java/org/jbehave/core/steps/Step.java
+++ jbehave-core/src/main/java/org/jbehave/core/steps/Step.java
@@ -2,16 +2,16 @@ package org.jbehave.core.steps;
 
 import org.jbehave.core.configuration.Keywords;
 import org.jbehave.core.failures.UUIDExceptionWrapper;
+import org.jbehave.core.reporters.StoryReporter;
 
 /**
  * A Step represents a runnable portion of a Scenario, which matches methods annotated in {@link CandidateSteps} instances.
  */
 public interface Step {
 
-    StepResult perform(UUIDExceptionWrapper storyFailureIfItHappened);
+    StepResult perform(StoryReporter storyReporter, UUIDExceptionWrapper storyFailureIfItHappened);
 
-    StepResult doNotPerform(UUIDExceptionWrapper storyFailureIfItHappened);
+    StepResult doNotPerform(StoryReporter storyReporter, UUIDExceptionWrapper storyFailureIfItHappened);
 
     String asString(Keywords keywords);
-    
 }
