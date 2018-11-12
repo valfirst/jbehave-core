296ec1eb94335b772979828b1e2e2ca53fc55d50
Valery Yatsynovich <valfirst@yandex.ru>
2018-11-13 00:24:18 +0300

    Expand StepMonitor API to report before/after step execution

--- jbehave-core/src/main/java/org/jbehave/core/steps/StepCreator.java
+++ jbehave-core/src/main/java/org/jbehave/core/steps/StepCreator.java
@@ -810,6 +810,7 @@ public class StepCreator {
             try {
                 parametriseStep();
                 stepMonitor.performing(parametrisedStep, dryRun);
+                stepMonitor.beforePerforming(parametrisedStep, dryRun, method);
                 if (!dryRun && method != null) {
                     Object outputObject = method.invoke(stepsInstance(), convertedParameters);
                     storeOutput(outputObject, method);
@@ -835,6 +836,9 @@ public class StepCreator {
             } catch (Throwable t) {
                 return failed(stepAsString, new UUIDExceptionWrapper(stepAsString, t)).withParameterValues(
                         parametrisedStep).setTimings(timer.stop());
+            }
+            finally {
+                stepMonitor.afterPerforming(parametrisedStep, dryRun, method);
             }
         }
 
